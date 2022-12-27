8873b74ae89c15006c047e4be436d6edf651b7bf
Diogo Pereira <90282042+diogopdev@users.noreply.github.com>
2022-12-27 03:37:52 +0000

    Created UI with gameplay
    Moved the gameplay from console to a UI

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 let playerPoints = 0;
 let computerPoints = 0;
 
+
+const clickRock = document.querySelector('#rock');
+clickRock.addEventListener('click', () => {
+	playRound("rock");
+});
+const clickPaper = document.querySelector('#paper');
+clickPaper.addEventListener('click', () => {
+	playRound("paper");
+});
+const clickScissors = document.querySelector('#scissors');
+clickScissors.addEventListener('click', () => {
+	playRound("scissors");
+});
+
+
+
 function getComputerChoice() {
 
 	let computerChoice = (Math.floor(Math.random() * (3 - 1 + 1) + 1));
@@ -15,12 +31,36 @@ function getComputerChoice() {
 	}
 }
 
-function playRound() {
 
-	const playerChoice = prompt("Choose your option between (Rock, Paper or Scissors):").toLowerCase();
+
+function checkAndReset(playerScore, computerScore, speaker) {
+	if (playerPoints === 5 || computerPoints === 5) {
+		console.log("entrei")
+		playerPoints = 0;
+		playerScore.textContent = playerPoints;
+		computerPoints = 0;
+		computerScore.textContent = computerPoints;
+
+		while (speaker.firstChild) {
+			speaker.removeChild(speaker.lastChild);
+		}
+	}
+}
+
+
+
+function playRound(playerChoice) {
 
 	const computerChoice = getComputerChoice().toLowerCase();
+	const speaker = document.querySelector('#speaker');
+	const speakerContent = document.createElement('div');
+	const playerScore = document.querySelector('#player-score');
+	const computerScore = document.querySelector('#computer-score');
 	let result;
+
+	checkAndReset(playerScore, computerScore, speaker);
+
+	//Gives the result
 
 	switch (playerChoice){
 		case "rock":
@@ -52,29 +92,44 @@ function playRound() {
 			break;
 	}
 
+	// Checks who won and writes the round log
+
 	if (result === "WIN") {
 		playerPoints++;
-		return("You won the round! " + playerChoice + " beats " + computerChoice);
+		playerScore.textContent = playerPoints;
+		speakerContent.textContent = "You won the round! " + playerChoice + " beats " + computerChoice;
+		speakerContent.style = "color: green";
+
 	} else if (result === "DRAW") {
-		return("This round it's a DRAW! Both players chose " + playerChoice);
+		speakerContent.textContent = "This round it's a DRAW! Both players chose " + playerChoice;
+
 	} else if (result === "LOSE") {
 		computerPoints++;
-		return("You lost the round! " + computerChoice + " beats " + playerChoice);
-	}
-}
-
-function game() {
-	for(i = 1; i <= 5; i++){
-		console.log(playRound());
-		console.log("Player points: " + playerPoints);
-		console.log("Computer points: " + computerPoints);
+		computerScore.textContent = computerPoints;
+		speakerContent.textContent = "You lost the round! " + computerChoice + " beats " + playerChoice;
+		speakerContent.style = "color: red";
 	}
 
-	if (playerPoints > computerPoints){
-		console.log("YOU WON THE GAME! " + playerPoints + " to " + computerPoints);
-	} else if (playerPoints < computerPoints) {
-		console.log("YOU LOST GAME! " + playerPoints + " to " + computerPoints);
-	} else {
-		console.log("It's a DRAW! " + playerPoints + " to " + computerPoints)
+	speaker.appendChild(speakerContent);
+
+	// Checks if anyone won, if it's true, removes rounds logs to print a "you win" or "you lost"
+
+	if (playerPoints === 5) {
+		while (speaker.firstChild) {
+			speaker.removeChild(speaker.lastChild);
+		}
+
+		speakerContent.textContent = "YOU WON!";
+		speakerContent.style = "color: green";
+		speaker.appendChild(speakerContent);
+
+	} else if (computerPoints === 5) {
+
+		while (speaker.firstChild) {
+			speaker.removeChild(speaker.lastChild);
+		}
+		speakerContent.textContent = "YOU LOST!";
+		speakerContent.style = "color: red";
+		speaker.appendChild(speakerContent);
 	}
 }
